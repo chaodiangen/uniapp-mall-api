354187abfb923ba1668dc3ad788965f4ab5481d8
chao.diangen <chao.diangen@trs.com.cn>
2022-09-15 18:02:37 +0800

    数据

--- src/modules/commodity/index_list/index_list.service.ts
+++ src/modules/commodity/index_list/index_list.service.ts
@@ -375,6 +375,36 @@ export class IndexListService {
             },
           ],
         },
+        {
+          type: 'commoditList1',
+          data: [
+            {
+              id: 1,
+              title:
+                '海贼王索隆图片海贼王索隆图片海贼王索隆图片海贼王索隆图片海贼王索隆图片海贼王索隆图片',
+              url: 'https://img.bizhiku.net/uploads/2022/0809/wcxs5yv2h43.jpg?x-oss-process=style/w_870-h_870',
+              pprice: '889',
+              oprice: '1089',
+              discount: '5.2',
+            },
+            {
+              id: 2,
+              title: '海贼王索隆图片',
+              url: 'https://desk-fd.zol-img.com.cn/t_s960x600c5/g5/M00/0D/09/ChMkJl3c9huIXs89AAR-50HaEV8AAvboAOtTP4ABH7_855.jpg',
+              pprice: '589',
+              oprice: '989',
+              discount: '7.2',
+            },
+            {
+              id: 3,
+              title: '海贼王娜美壁纸',
+              url: 'https://desk-fd.zol-img.com.cn/t_s960x600c5/g5/M00/0D/04/ChMkJ13bqqCISyNJAARuw8uAcSgAAvaWwGxA9IABG7b911.jpg',
+              pprice: '889',
+              oprice: '1089',
+              discount: '5.2',
+            },
+          ],
+        },
       ],
     });
   }
